25b26ee3366dacc4dfe11eb598c1f1396f66af83
sama717 <samafarag7@gmail.com>
2024-04-14 23:14:09 +0200

    Splash screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:authentication/Features/auth/registration/view/page/registration_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
+   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   MaterialApp materialApp = const MaterialApp(
     home: RegistrationPage(),
   );
   runApp(materialApp);
+  FlutterNativeSplash.remove();
 }
