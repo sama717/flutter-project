4d1fe12cfee7ecf78a5f265260897187975a1f0f
sama717 <samafarag7@gmail.com>
2024-05-02 17:22:54 +0300

    Minor changes added

--- lib/Features/auth/verify/view/components/bot_verify.dart
+++ lib/Features/auth/verify/view/components/bot_verify.dart
@@ -1,29 +1,41 @@
 import 'package:authentication/Features/auth/login/view/page/login_page.dart';
+import 'package:authentication/Features/auth/verify/controller/cubit/verification_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class VerifyBotNav extends StatelessWidget {
   const VerifyBotNav({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: FilledButton(
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(Colors.blueAccent),
-            ),
-            onPressed: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const LoginPage()),
-                );
-            },
-            child: const Text("Confirm"),
-          ),
-        ),
-      ],
+    return BlocProvider(
+      create: (context) => VerificationCubit(),
+      child: BlocBuilder<VerificationCubit, VerificationState>(
+        builder: (context, state) {
+          final VerificationCubit cubit = context.read<VerificationCubit>();
+          return Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: FilledButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(Colors.blueAccent),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const LoginPage()),
+                    );
+                  },
+                  child: const Text("Confirm"),
+                ),
+              ),
+            ],
+          );
+        },
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:authentication/Features/auth/login/view/page/login_page.dart';
 import 'package:authentication/Features/auth/onboarding/view/page/onboarding_page.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -11,8 +13,13 @@ void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   MaterialApp materialApp = MaterialApp(
+    builder: DevicePreview.appBuilder,
+    useInheritedMediaQuery: true,
     home: onBoarding ? const LoginPage() : const OnBoardingPage(),
   );
-  runApp(materialApp);
+  runApp(DevicePreview(
+    enabled: true,
+    builder: (context) => materialApp, // Wrap your app
+  ),);
   FlutterNativeSplash.remove();
 }
